d17fa17d2d8f0ec86f2580670cfc6fcb2f008a6b
yarabramasta <yarabram111@gmail.com>
2022-06-05 17:20:53 +0700

    feat: update bspwm & dunst

---
+++ dunst/.config/dunst/reload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#########################################
+# SHAMELESS RIPOFF FROM ANOTHER GUY LUL #
+#########################################
+
+pkill dunst
+dunst -config ~/.config/dunst/dunstrc &
