c8dbeedafb051b329cdf00fb6be4c3e7e0a79a90
yarabramasta <yarabram111@gmail.com>
2022-06-06 11:49:44 +0700

    feat(fish): add aliases

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,19 @@
+alias chmox='chmod +x'
+
+alias where=which # sometimes i forget
+
+# typos
+abbr bwre brew
+abbr gti git
+abbr yearn yarn
+abbr clera clear
+
+alias diskspace_report="df -P -kHl"
+alias free_diskspace_report="diskspace_report"
+
+# shortcuts
+alias v="nvim"
+alias vs="sudo nvim"
+
+# File size
+alias fs="stat -f \"%z bytes\""
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ set default_user "yarabramasta"
 set default_machine "yb"
 
 source ~/.config/fish/functions.fish
+source ~/.config/fish/aliases.fish
+source ~/.config/fish/paths.fish
 
 set -x PATH $PATH /sbin/
 
@@ -28,5 +30,17 @@ set -g fish_color_valid_path --underline
 
 set -g fish_color_dimmed 555
 set -g fish_color_separator 999
+
+# Local prompt customization
+set fish_greeting
+
+# highlighting inside manpages and elsewhere
+set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -gx LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -gx LESS_TERMCAP_me \e'[0m'           # end mode
+set -gx LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -gx LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
+set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 
 starship init fish | source
